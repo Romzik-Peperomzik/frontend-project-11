1b8c8856b82bebdd9de1be83eee4568600e2b71b
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-11-02 22:19:00 +0300

    added ignore files, updated webpack.config.cjs

---
+++ webpack.config.cjs
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
