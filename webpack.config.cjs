70421f958dcc510a307fe03486d73a05f139bf00
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-11-06 04:39:04 +0300

    added webpack-dev-server

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,6 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
