91aeb9f937f530a9ec867940f91c4a18cad33309
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-11-22 20:18:21 +0300

    added i18next locales/ files

---
+++ src/locales/ru.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    invalidUrlError: 'Ссылка должна быть валидным URL',
+    existUrlError: 'RSS уже существует',
+    rssSuccessedLoad: 'RSS успешно загружен',
+  },
+};
