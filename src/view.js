7d01a37d6e3d60a00b95df015ee786f03eb9307d
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-11-22 20:22:05 +0300

    added render translated text with i18next and updated index.html

--- src/view.js
+++ src/view.js
@@ -1,30 +1,52 @@
 import onChange from 'on-change';
 
+const renderRssFormError = (state, elements, i18n) => {
+  const { errors } = state.rssForm;
+  const { feedback } = elements;
+  feedback.innerHTML = i18n.t(errors.message);
+  feedback.classList.add('text-danger');
+  feedback.classList.remove('text-success');
+};
+
 const renderRssForm = (state, elements) => {
-  const { form, input } = elements;
-  if (state.rssForm.status === 'invalid') {
-    input.classList.add('is-invalid');
-  } else {
-    input.classList.remove('is-invalid');
-    form.reset();
-    input.focus();
+  const { status } = state.rssForm;
+  const { form, input, feedback } = elements;
+  switch (status) {
+    case 'invalid':
+      input.classList.add('is-invalid');
+      break;
+    case 'valid':
+      input.classList.remove('is-invalid');
+      // Successfull url load
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      feedback.innerHTML = 'RSS успешно загружен'; // i18n.t(rssSuccessedLoad);
+      // ^ TODO: переместить строки в блок при успешной загрузке rss
+      form.reset();
+      input.focus();
+      break;
+    default:
+      throw new Error(`Unknown form render status: ${status}`);
   }
 };
 
-const render = (state, path, elements) => {
+const render = (state, path, elements, i18n) => {
   switch (path) {
     case 'rssForm.status':
-    case 'rssForm.errors':
-    case 'channels':
       renderRssForm(state, elements);
+      break;
+    case 'channels':
+      break;
+    case 'rssForm.errors':
+      renderRssFormError(state, elements, i18n);
       break;
     default:
       throw new Error(`Unknown state path: ${path}`);
   }
 };
 
-const view = (state, elements) => onChange(state, (path) => {
-  render(state, path, elements);
+const view = (state, elements, i18n) => onChange(state, (path) => {
+  render(state, path, elements, i18n);
 });
 
 export default view;
