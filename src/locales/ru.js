9ac9fce5132186ccd5afcd426f7f517d7355461e
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-11-26 23:35:22 +0300

    updated locales/ru.js

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,5 +3,10 @@ export default {
     invalidUrlError: 'Ссылка должна быть валидным URL',
     existUrlError: 'RSS уже существует',
     rssSuccessedLoad: 'RSS успешно загружен',
+    networkResponseError: 'Не удаётся обработать ответ сервера',
+    XMLdocumentParsingError: 'Ошибка парсинга документа, обновите страницу и попробуйте снова.',
+    postsTitle: 'Посты',
+    feedsTitle: 'Фиды',
+    btnWatchMore: 'Просмотр',
   },
 };
